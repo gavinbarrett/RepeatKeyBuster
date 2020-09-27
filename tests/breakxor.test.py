1089e475a2ddbb43e0a85b4362a0ea5b133e9a52
Gavin <gavinbrrtt@gmail.com>
2020-09-27 12:07:47 -0700

    Updated test ciphertext file path.

--- tests/breakxor.test.py
+++ tests/breakxor.test.py
@@ -7,7 +7,7 @@ class TesterBreakKey(unittest.TestCase):
 
 	def test_break_key(self):
 		expected = 'Terminator X: Bring the noise'
-		self.assertEqual(expected, crack_xor('./6.txt'))
+		self.assertEqual(expected, crack_xor('../src/6.txt'))
 
 if __name__ == "__main__":
 	unittest.main()
